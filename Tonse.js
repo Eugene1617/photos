2932fb495e933ebe472894488f7245f90bb91a52
Eugene1617 <kayangeeugene@gmail.com>
2025-09-10 08:16:51 +0200

    Add files via upload

---
+++ Tonse.js
@@ -0,0 +1,102 @@
+let photos = ['presse\\20250807_1511_Python Programming Lesson_simple_compose_01k22ag0deff5vm8ac18apnd83.png',
+'presse\\WhatsApp Image 2025-08-01 at 08.40.33_863e3304.jpg',
+'presse\\WhatsApp Image 2025-08-01 at 08.40.38_1ad9f815.jpg',
+'presse\\WhatsApp Image 2025-08-01 at 10.52.01_e5993616.jpg',
+'presse\\WhatsApp Image 2025-08-01 at 10.52.03_49c30af2.jpg',
+'presse\\WhatsApp Image 2025-08-01 at 10.52.05_efb98878.jpg',
+'presse\\WhatsApp Image 2025-08-01 at 10.52.22_744e3073.jpg',
+'presse\\WhatsApp Image 2025-08-01 at 10.52.25_2265175a.jpg',
+'presse\\WhatsApp Image 2025-08-01 at 10.52.29_b6d7e204.jpg',
+'presse\\WhatsApp Image 2025-08-01 at 10.52.34_d24dc78e.jpg',
+'presse\\WhatsApp Image 2025-08-01 at 10.52.38_6380a2dc.jpg',
+'presse\\WhatsApp Image 2025-08-01 at 10.52.41_14bd60a6.jpg',
+'presse\\WhatsApp Image 2025-08-01 at 10.52.41_b79fc89a.jpg',
+'presse\\WhatsApp Image 2025-08-01 at 10.52.46_e427b6ec.jpg',
+'presse\\WhatsApp Image 2025-08-01 at 10.52.55_da030b17.jpg',
+'presse\\WhatsApp Image 2025-08-01 at 10.52.56_dc500304.jpg',
+'presse\\WhatsApp Image 2025-08-01 at 10.53.00_42d54799.jpg',
+'presse\\WhatsApp Image 2025-08-01 at 10.58.09_86ae05ec.jpg',
+'presse\\WhatsApp Image 2025-08-01 at 11.15.02_a7c4b6e0.jpg',
+'presse\\WhatsApp Image 2025-08-01 at 11.15.11_c729789a.jpg',
+'presse\\WhatsApp Image 2025-08-01 at 11.16.14_f2b05cfe.jpg',
+'presse\\WhatsApp Image 2025-08-01 at 11.17.11_a7586e01.jpg',
+'presse\\WhatsApp Image 2025-08-01 at 11.22.38_cfab3ed1.jpg',
+'presse\\WhatsApp Image 2025-08-01 at 11.25.20_c63a084e.jpg',
+'presse\\WhatsApp Image 2025-08-01 at 11.32.01_a734e788.jpg',
+'presse\\WhatsApp Image 2025-08-01 at 14.06.59_7c2bbbf5.jpg',
+'presse\\WhatsApp Image 2025-08-01 at 14.07.03_a86b83dd.jpg',
+'presse\\WhatsApp Image 2025-08-01 at 14.08.41_bf26ad8e.jpg',
+'presse\\WhatsApp Image 2025-08-01 at 14.08.47_46ac94d2.jpg',
+'presse\\WhatsApp Image 2025-08-01 at 14.08.55_57a27454.jpg',
+'presse\\WhatsApp Image 2025-08-01 at 14.09.11_9bdd823b.jpg',
+'presse\\WhatsApp Image 2025-08-01 at 14.09.12_889a3666.jpg',
+'presse\\WhatsApp Image 2025-08-01 at 14.09.29_ba1d0e09.jpg',
+'presse\\WhatsApp Image 2025-08-01 at 14.09.50_ec357044.jpg',
+'presse\\WhatsApp Image 2025-08-01 at 14.10.12_fd11cb3c.jpg',
+'presse\\WhatsApp Image 2025-08-01 at 14.10.18_3860b94d.jpg',
+'presse\\WhatsApp Image 2025-08-01 at 14.10.26_27be2cd4.jpg',
+'presse\\WhatsApp Image 2025-08-01 at 14.11.36_6d8cb349.jpg',
+'presse\\WhatsApp Image 2025-08-01 at 14.44.27_afe39d7a.jpg',
+'presse\\WhatsApp Image 2025-08-01 at 14.44.28_230e77bc.jpg',
+'presse\\WhatsApp Image 2025-08-01 at 14.44.30_8b7c7629.jpg',
+'presse\\WhatsApp Image 2025-08-01 at 14.44.31_e6ac98aa.jpg',
+'presse\\WhatsApp Image 2025-08-01 at 14.44.32_0dd5c731.jpg',
+'presse\\WhatsApp Image 2025-08-01 at 14.44.33_3ad8814d.jpg',
+'presse\\WhatsApp Image 2025-08-01 at 14.44.34_62bbe6b6.jpg',
+'presse\\WhatsApp Image 2025-08-01 at 14.44.35_cd069b35.jpg',
+'presse\\WhatsApp Image 2025-08-01 at 14.44.36_e74867b5.jpg',
+'presse\\WhatsApp Image 2025-08-01 at 14.44.37_797a2638.jpg',
+'presse\\WhatsApp Image 2025-08-01 at 14.44.52_88452e19.jpg',
+'presse\\WhatsApp Image 2025-08-01 at 14.44.55_888c837f.jpg',
+'presse\\WhatsApp Image 2025-08-01 at 14.44.56_0d347769.jpg',
+'presse\\WhatsApp Image 2025-08-01 at 14.44.57_df48754b.jpg',
+'presse\\WhatsApp Image 2025-08-01 at 14.44.59_2be64f8f.jpg',
+'presse\\WhatsApp Image 2025-08-01 at 14.45.01_875697c4.jpg',
+'presse\\WhatsApp Image 2025-08-01 at 14.45.03_921168a6.jpg',
+'presse\\WhatsApp Image 2025-08-01 at 14.45.05_e957af7c.jpg',
+'presse\\WhatsApp Image 2025-08-01 at 14.45.07_f3b73604.jpg',
+'presse\\WhatsApp Image 2025-08-01 at 14.49.30_d094afb4.jpg',
+'presse\\WhatsApp Image 2025-08-01 at 14.49.31_180e6f8d.jpg',
+'presse\\WhatsApp Image 2025-08-01 at 14.49.33_0ed7ad74.jpg',
+'presse\\WhatsApp Image 2025-08-01 at 14.49.36_88d9ec6b.jpg',
+'presse\\WhatsApp Image 2025-08-01 at 14.49.37_666cdf50.jpg',
+'presse\\WhatsApp Image 2025-08-01 at 14.49.41_898543f4.jpg',
+'presse\\WhatsApp Image 2025-08-01 at 14.49.43_64cf6f22.jpg',
+'presse\\WhatsApp Image 2025-08-01 at 14.50.16_43925535.jpg',
+'presse\\WhatsApp Image 2025-08-01 at 14.50.17_b385dfc5.jpg',
+'presse\\WhatsApp Image 2025-08-01 at 14.50.19_bcceeede.jpg',
+'presse\\WhatsApp Image 2025-08-01 at 14.50.24_eac45f3c.jpg',
+'presse\\WhatsApp Image 2025-08-01 at 15.00.47_f50b7050.jpg',
+'presse\\WhatsApp Image 2025-08-01 at 17.45.07_2562c63a.jpg',
+'presse\\WhatsApp Image 2025-08-01 at 17.45.27_c85f16fd.jpg',
+'presse\\WhatsApp Image 2025-08-01 at 17.45.36_2d22d291.jpg',
+'presse\\WhatsApp Image 2025-08-01 at 17.45.36_f0b27a53.jpg',
+'presse\\WhatsApp Image 2025-08-01 at 17.45.41_fa827dae.jpg',
+'presse\\WhatsApp Image 2025-08-01 at 17.46.37_6caaba47.jpg',
+'presse\\WhatsApp Image 2025-08-01 at 17.46.38_2279cd6d.jpg',
+'presse\\WhatsApp Image 2025-08-01 at 17.46.38_d1af60bd.jpg',
+'presse\\WhatsApp Image 2025-08-01 at 17.50.47_db49fe77.jpg'];
+let number =0;
+let photos_id = document.getElementById("photos");
+function photos1() {
+    
+    photos_id.src=photos[number];
+    number +=1;
+    if (number>photos.length-1){
+        alert("no more photos");
+  
+}
+else{
+    photos_id.src=photos[number];
+}}
+
+function photos2(){
+  
+if (number<0){
+    alert("no more photos");
+
+}
+else{
+    number -=1;
+    photos_id.src=photos[number];
+}}
